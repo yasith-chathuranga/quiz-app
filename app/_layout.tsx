1fb9cd777a8fe389731c1b5e0f8cf4acec8db99a
yasith-chathuranga <yasithchathuranga999@gmail.com>
2025-02-28 20:04:39 +0530

    do some changes and remove some unused things

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,6 @@ export default function RootLayout() {
             <Stack.Screen name="index" options={{ title: "Home" }} />
             <Stack.Screen name="modules" options={{ title: "Modules" }} />
             <Stack.Screen name="quiz" options={{ title: "Quiz" }} />
-            <Stack.Screen name="results" options={{ title: "Results" }} />
         </Stack>
     );
 }
